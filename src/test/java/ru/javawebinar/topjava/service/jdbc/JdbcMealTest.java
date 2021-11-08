c18a4dd975b586263fb23ad5fcd3c6d77169e49a
sergias <sergias@ukr.net>
2021-11-08 11:21:28 +0200

    hw05 - 3: Сделать тесты всех реализаций (jdbc, jpa, datajpa) через наследование (без дублирования)

---
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealTest.java
@@ -0,0 +1,92 @@
+package ru.javawebinar.topjava.service.jdbc;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.MealServiceTest;
+
+@ActiveProfiles({Profiles.JDBC, Profiles.DB_IMPLEMENTATION})
+@RunWith(SpringRunner.class)
+public class JdbcMealTest extends MealServiceTest {
+
+    @Test
+    @Override
+    public void delete() {
+        super.delete();
+    }
+
+    @Test
+    @Override
+    public void deleteNotFound() {
+        super.deleteNotFound();
+    }
+
+    @Test
+    @Override
+    public void deleteNotOwn() {
+        super.deleteNotOwn();
+    }
+
+    @Test
+    @Override
+    public void create() {
+        super.create();
+    }
+
+    @Test
+    @Override
+    public void duplicateDateTimeCreate() {
+        super.duplicateDateTimeCreate();
+    }
+
+    @Test
+    @Override
+    public void get() {
+        super.get();
+    }
+
+    @Test
+    @Override
+    public void getNotFound() {
+        super.getNotFound();
+    }
+
+    @Test
+    @Override
+    public void getNotOwn() {
+        super.getNotOwn();
+    }
+
+    @Test
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Test
+    @Override
+    public void updateNotOwn() {
+        super.updateNotOwn();
+    }
+
+    @Test
+    @Override
+    public void getAll() {
+        super.getAll();
+    }
+
+    @Test
+    @Override
+    public void getBetweenInclusive() {
+        super.getBetweenInclusive();
+    }
+
+    @Test
+    @Override
+    public void getBetweenWithNullDates() {
+        super.getBetweenWithNullDates();
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserTest.java
@@ -0,0 +1,73 @@
+package ru.javawebinar.topjava.service.jpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.UserServiceTest;
+
+@ActiveProfiles({Profiles.JPA, Profiles.DB_IMPLEMENTATION})
+@RunWith(SpringRunner.class)
+public class JpaUserTest extends UserServiceTest {
+
+    @Test
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Test
+    @Override
+    public void create() {
+        super.create();
+    }
+
+    @Test
+    @Override
+    public void duplicateMailCreate() {
+        super.duplicateMailCreate();
+    }
+
+    @Test
+    @Override
+    public void delete() {
+        super.delete();
+    }
+
+    @Test
+    @Override
+    public void deletedNotFound() {
+        super.deletedNotFound();
+    }
+
+    @Test
+    @Override
+    public void get() {
+        super.get();
+    }
+
+    @Test
+    @Override
+    public void getNotFound() {
+        super.getNotFound();
+    }
+
+    @Test
+    @Override
+    public void getByEmail() {
+        super.getByEmail();
+    }
+
+    @Test
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Test
+    @Override
+    public void getAll() {
+        super.getAll();
+    }
+}
